cfd1bea7d5a01d493897333934fbb0fff9cd2396
LouiseALD <louisecarvalho2001@gmail.com>
2019-06-09 23:09:23 -0300

    segundo commit

---
+++ URI/alfabeto.java
@@ -0,0 +1,17 @@
+// Printa as letras do alfabeto
+
+
+package obitreino2019;
+import java.util.Scanner;
+public class H {
+	public static void main (String[]args) {
+		    char letra = 'a';
+		    while(letra <='z') { //  pode ser tbm letra !='z'
+		        System.out.println(letra+" ");
+		        letra++;
+
+		    }
+		}
+		
+		
+	}
